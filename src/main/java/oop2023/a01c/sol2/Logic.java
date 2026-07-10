a7b76bd869c369d1d5a666fe34ff26f4a6122221
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2026-07-10 17:23:44 +0200

    feat: add exercise code

---
+++ src/main/java/oop2023/a01c/sol2/Logic.java
@@ -0,0 +1,12 @@
+package oop2023.a01c.sol2;
+
+import java.util.Optional;
+
+public interface Logic {
+
+    Optional<Integer> hit(Position position); 
+
+    Optional<Integer> getMark(Position position);
+
+    boolean isOver();
+}
---
+++ src/main/java/oop2023/a01c/sol2/Position.java
@@ -0,0 +1,4 @@
+package oop2023.a01c.sol2;
+
+public record Position(int x, int y){
+}
